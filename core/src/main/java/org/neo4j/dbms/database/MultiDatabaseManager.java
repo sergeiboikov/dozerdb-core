90cdf02483c17743edc6e4447af5e7c43bd8635a
John Mark Suhy <jmsuhy@dozerdb.org>
2024-10-29 12:40:28 -0400

    Updated version to 5.24.2-1.1.0-alpha.1 - to address new multidb features. (semantic versioning on 1.x.x part).

--- core/src/main/java/org/neo4j/dbms/database/MultiDatabaseManager.java
+++ core/src/main/java/org/neo4j/dbms/database/MultiDatabaseManager.java
@@ -265,11 +265,11 @@ public final class MultiDatabaseManager {
         this.stopDatabase(context);
     }
 
-
     private void checkDatabaseLimit(NamedDatabaseId namedDatabaseId) {
 
         // Default to 100 if the max databases is not set in the configuration.
-        Integer maxDatabases = Optional.ofNullable(config.get(DozerDbSettings.max_databases)).orElse(100);
+        Integer maxDatabases =
+                Optional.ofNullable(config.get(DozerDbSettings.max_databases)).orElse(100);
         if (databaseRepository.registeredDatabases().size() >= maxDatabases) {
             throw new DatabaseManagementException("Could not create gdb: " + namedDatabaseId.name()
                     + " because you have exceeded the limit of " + maxDatabases + ".");
